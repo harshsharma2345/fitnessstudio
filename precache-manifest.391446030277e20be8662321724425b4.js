fee7411bad6a12658e552cfcfdd27f1f3898c8dc
Harsh Sharma <Harshsharma2345@gmail.com>
2020-12-03 10:59:24 +0530

    Updates

---
+++ precache-manifest.391446030277e20be8662321724425b4.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b97baee2c12a0385eaab5f5daf2f53b",
+    "url": "/fitnessstudio/index.html"
+  },
+  {
+    "revision": "6e3fb94c7d8591b1aeb5",
+    "url": "/fitnessstudio/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "a84a938649e9a72c308f",
+    "url": "/fitnessstudio/static/js/2.49f3cfe9.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/fitnessstudio/static/js/2.49f3cfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3fb94c7d8591b1aeb5",
+    "url": "/fitnessstudio/static/js/main.49e4cf4e.chunk.js"
+  },
+  {
+    "revision": "f3258510e89ae09173c8",
+    "url": "/fitnessstudio/static/js/runtime-main.a5f5f464.js"
+  },
+  {
+    "revision": "f6dd8b2d204206b82a74bdd0782c9326",
+    "url": "/fitnessstudio/static/media/Aerobics.f6dd8b2d.jpg"
+  },
+  {
+    "revision": "5fde5125932d44d3bc92a48723c3566f",
+    "url": "/fitnessstudio/static/media/File1.5fde5125.jpg"
+  },
+  {
+    "revision": "c921dfbcd7563398d6fbd4a8c56579dc",
+    "url": "/fitnessstudio/static/media/File2.c921dfbc.jpg"
+  },
+  {
+    "revision": "290bbfb8df60f3d0981da696761f2bf4",
+    "url": "/fitnessstudio/static/media/File3.290bbfb8.jpg"
+  },
+  {
+    "revision": "2848219cad5f6f108c7cd71fab24f440",
+    "url": "/fitnessstudio/static/media/File4.2848219c.jpg"
+  },
+  {
+    "revision": "fd0d9324eb2a24f183cf4113b47b477b",
+    "url": "/fitnessstudio/static/media/File5.fd0d9324.jpg"
+  },
+  {
+    "revision": "376f5732304842024a058629d91f5acf",
+    "url": "/fitnessstudio/static/media/File6.376f5732.jpg"
+  },
+  {
+    "revision": "05ec7262db0122a98cb4418b1af9285d",
+    "url": "/fitnessstudio/static/media/File7.05ec7262.jpg"
+  },
+  {
+    "revision": "8410f142f3cff9e48fd9f67de435df69",
+    "url": "/fitnessstudio/static/media/File8.8410f142.jpg"
+  },
+  {
+    "revision": "5d9d43611b1ed307772f9a7a2b5e0ef8",
+    "url": "/fitnessstudio/static/media/File9.5d9d4361.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/fitnessstudio/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/fitnessstudio/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/fitnessstudio/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/fitnessstudio/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/fitnessstudio/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/fitnessstudio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "e4644451fed24dc0f4e86b3d5325e036",
+    "url": "/fitnessstudio/static/media/boxing.e4644451.jpg"
+  },
+  {
+    "revision": "0443c686eba4db944baf0770d2271154",
+    "url": "/fitnessstudio/static/media/cardio.0443c686.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/fitnessstudio/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/fitnessstudio/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/fitnessstudio/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/fitnessstudio/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "ac50f550d99da6671fc9083df06d9d3e",
+    "url": "/fitnessstudio/static/media/fitbg1.ac50f550.jpg"
+  },
+  {
+    "revision": "921cc0a7c0876d534a550d6cc1c6274e",
+    "url": "/fitnessstudio/static/media/image10.921cc0a7.jpg"
+  },
+  {
+    "revision": "8d1b42cea2bb8962a8adc65a6b8f0380",
+    "url": "/fitnessstudio/static/media/image5.8d1b42ce.jpg"
+  },
+  {
+    "revision": "1d8310591ff28eb52448c63761e819dd",
+    "url": "/fitnessstudio/static/media/image6.1d831059.jpg"
+  },
+  {
+    "revision": "d45ea045e1e115584729104b18d7617d",
+    "url": "/fitnessstudio/static/media/image7.d45ea045.jpg"
+  },
+  {
+    "revision": "f6639570e3174e6caf6b054a5d2867c2",
+    "url": "/fitnessstudio/static/media/image8.f6639570.jpg"
+  },
+  {
+    "revision": "140b4515aa1b1b8ca7949aa9dc7d25a3",
+    "url": "/fitnessstudio/static/media/image9.140b4515.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/fitnessstudio/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/fitnessstudio/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "f8d566c70d47bdffc794808ac36a2a28",
+    "url": "/fitnessstudio/static/media/logo.f8d566c7.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/fitnessstudio/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "286c7fe2ea2a672f6e073cab51c3b6e6",
+    "url": "/fitnessstudio/static/media/nutrition.286c7fe2.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/fitnessstudio/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/fitnessstudio/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/fitnessstudio/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "8af84ce6336427d69d8a8a9b3bc12ecd",
+    "url": "/fitnessstudio/static/media/strength.8af84ce6.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/fitnessstudio/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/fitnessstudio/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/fitnessstudio/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/fitnessstudio/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/fitnessstudio/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "b1ba14a649d91db8a0f58bf226148eb6",
+    "url": "/fitnessstudio/static/media/zumba.b1ba14a6.jpg"
+  }
+]);
